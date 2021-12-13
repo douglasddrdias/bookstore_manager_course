f2df0cfb6fa8182abb798ff87b9a09f05de61454
Douglas Dias <douglasddrdias@gmail.com>
2021-12-13 15:29:00 -0300

    Refactores functionality for book creation

---
+++ src/main/java/com/douglasdias/bookstoremanager/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.douglasdias.bookstoremanager.controller;
+
+import com.douglasdias.bookstoremanager.dto.MessageResponseDTO;
+import com.douglasdias.bookstoremanager.entity.Book;
+import com.douglasdias.bookstoremanager.repository.BookRepository;
+import com.douglasdias.bookstoremanager.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody Book book){
+        return bookService.create(book);
+    }
+}
---
+++ src/main/java/com/douglasdias/bookstoremanager/service/BookService.java
@@ -0,0 +1,25 @@
+package com.douglasdias.bookstoremanager.service;
+
+import com.douglasdias.bookstoremanager.dto.MessageResponseDTO;
+import com.douglasdias.bookstoremanager.entity.Book;
+import com.douglasdias.bookstoremanager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public MessageResponseDTO create(Book book){
+        Book savedBook = bookRepository.save(book);
+        return MessageResponseDTO.builder()
+                .message("Book created with ID " + savedBook.getId())
+                .build();
+    }
+}
